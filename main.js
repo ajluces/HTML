9962b66e57cb1e67d99294fce216a21b53ff1906
ajluces <andrew.julao.cvsu.edu.ph>
2020-10-23 18:58:02 +0800

    HTML

---
+++ main.js
@@ -0,0 +1,98 @@
+console.log('Hello World!');
+//document.write("hello world");
+/*switch (date) {
+  case 1:
+    document.write("monday");
+    break;
+  case 4:
+    document.write("tuesday");
+    break;
+  default:
+  document.write("error");
+}
+*/
+/*var i = 1;
+for (; i<=10; i++){
+  if (i==6)
+  {
+    continue;
+  }
+document.write(i);
+}*/
+/*function tst(name, age) {
+  alert("hi im " + name + age + " years old" );
+}
+tst("aj ", 18);*/
+/*var y = confirm("enter your name ");
+if (y==true) {
+  alert("ty");
+}
+else {
+  alert("fhh");
+}*/
+/*var person = ["andrew", "19"];
+var like = ["play"];
+var comb = person.concat(like);
+document.write(comb);
+document.body.innerHTML = "HELLO WORLD";
+*/
+var uname = "andrew";
+var pass = "wala";
+function validate() {
+  var n1 = document.getElementById("num1");
+  var n2 = document.getElementById("num2");
+  if (n1.value == uname && n2.value == pass)
+  alert("true");
+  else {
+  alert("false");
+  }
+}
+console.log('Hello World!');
+//document.write("hello world");
+/*switch (date) {
+  case 1:
+    document.write("monday");
+    break;
+  case 4:
+    document.write("tuesday");
+    break;
+  default:
+  document.write("error");
+}
+*/
+/*var i = 1;
+for (; i<=10; i++){
+  if (i==6)
+  {
+    continue;
+  }
+document.write(i);
+}*/
+/*function tst(name, age) {
+  alert("hi im " + name + age + " years old" );
+}
+tst("aj ", 18);*/
+/*var y = confirm("enter your name ");
+if (y==true) {
+  alert("ty");
+}
+else {
+  alert("fhh");
+}*/
+/*var person = ["andrew", "19"];
+var like = ["play"];
+var comb = person.concat(like);
+document.write(comb);
+document.body.innerHTML = "HELLO WORLD";
+*/
+var uname = "andrew";
+var pass = "wala";
+function validate() {
+  var n1 = document.getElementById("num1");
+  var n2 = document.getElementById("num2");
+  if (n1.value == uname && n2.value == pass)
+  alert("true");
+  else {
+  alert("false");
+  }
+}
